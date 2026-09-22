8d395632e9724a20347b63fd3a6294f4540b8201
Limb-V <v.alimbochko@gmail.com>
2026-09-22 17:35:44 +0100

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,10 @@
+variable = 1
+print(type(variable))
+variable = 1.01
+print(type(variable))
+variable = True
+print(type(variable))
+variable = "Hello World!"
+print(type(variable))
+variable = []
+print(type(variable))
